f8fb16ba1191c1f2b0ebe93a6ac6b4530734bcbe
John Booth <jhunter.booth@gmail.com>
2022-05-24 10:01:59 -0600

    blocking code and clean async

--- starter/shoppingCart.js
+++ starter/shoppingCart.js
@@ -2,6 +2,13 @@
 
 console.log("Exporting module");
 
+// Blocking code --------------------------------------------------
+// console.log("Start fetching users");
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log("Finish fetching users")
+//-----------------------------------------------------------------
+
 const shippingCost = 10;
 export const cart = [];
 
